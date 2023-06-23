d46713f503f16b9c3e2f7092f09c9d0766098259
Mathis <mathis.fullriede@gmail.com>
2023-06-23 18:46:07 +0200

    ui work

---
+++ app/lookup/[domain]/page.jsx
@@ -0,0 +1,214 @@
+import ReactCountryFlag from 'react-country-flag';
+import whoiser from 'whoiser';
+
+import DnsTable from '@/components/DnsTable';
+import DomainNotRegistered from '@/components/DomainNotRegistered';
+import { isAvailable } from '@/lib/whois';
+import DnsLookup from '@/utils/DnsLookup';
+
+export const fetchCache = 'default-no-store';
+
+const LookupDomain = async ({ params: { domain } }) => {
+  const records = await DnsLookup.resolveAllRecords(domain);
+
+  const whoisResult = whoiser.firstResult(
+    await whoiser(domain, {
+      timeout: 3000,
+    })
+  );
+
+  console.log(whoisResult);
+
+  if ((await isAvailable(domain)) !== 'registered') {
+    return <DomainNotRegistered />;
+  }
+
+  return (
+    <>
+      <div className="grid gap-4 md:grid-cols-4">
+        <div className="md:col-span-2 h-full">
+          <div className="h-full rounded-xl bg-slate-100 px-8 py-5 dark:bg-slate-950 md:flex">
+            <div>
+              <span className="rounded-lg bg-slate-200 px-2 py-1 text-sm font-extrabold uppercase text-slate-950 dark:bg-slate-900 dark:text-slate-50">
+                Dates
+              </span>
+            </div>
+            <div className="grid gap-4 md:m-auto md:grid-cols-3">
+              {whoisResult['Created Date'] ? (
+              <div className="flex flex-col text-center">
+                <p className="text-2xl font-bold text-slate-900 dark:text-slate-100">
+                  {new Date(whoisResult['Created Date']).toLocaleDateString(
+                    'en-US',
+                    {
+                      month: 'long',
+                      day: 'numeric',
+                      year: 'numeric',
+                    }
+                  )}
+                </p>
+                <p className="text-lg font-light text-slate-900 dark:text-slate-100">
+                  {new Date(whoisResult['Created Date']).toLocaleTimeString(
+                    'en-US',
+                    {
+                      hour: 'numeric',
+                      minute: 'numeric',
+                    }
+                  )}
+                </p>
+                <p className="mt-2 text-lg font-semibold text-slate-800 dark:text-slate-200">
+                  Domain registered
+                </p>
+              </div>
+              ) : null}
+              {whoisResult['Updated Date'] ? (
+              <div className="flex flex-col text-center">
+                <p className="text-2xl font-bold text-slate-900 dark:text-slate-100">
+                  {new Date(whoisResult['Updated Date']).toLocaleDateString(
+                    'en-US',
+                    {
+                      month: 'long',
+                      day: 'numeric',
+                      year: 'numeric',
+                    }
+                  )}
+                </p>
+                <p className="text-lg font-light text-slate-900 dark:text-slate-100">
+                  {new Date(whoisResult['Updated Date']).toLocaleTimeString(
+                    'en-US',
+                    {
+                      hour: 'numeric',
+                      minute: 'numeric',
+                    }
+                  )}
+                </p>
+                <p className="mt-2 text-lg font-semibold text-slate-800 dark:text-slate-200">
+                  Updated
+                </p>
+              </div>
+              ) : null}
+              {whoisResult['Expiry Date'] ? (
+              <div className="flex flex-col text-center">
+                <p className="text-2xl font-bold text-slate-900 dark:text-slate-100">
+                  {new Date(whoisResult['Expiry Date']).toLocaleDateString(
+                    'en-US',
+                    {
+                      month: 'long',
+                      day: 'numeric',
+                      year: 'numeric',
+                    }
+                  )}
+                </p>
+                <p className="text-lg font-light text-slate-900 dark:text-slate-100">
+                  {new Date(whoisResult['Expiry Date']).toLocaleTimeString(
+                    'en-US',
+                    {
+                      hour: 'numeric',
+                      minute: 'numeric',
+                    }
+                  )}
+                </p>
+                <p className="mt-2 text-lg font-semibold text-slate-800 dark:text-slate-200">
+                  Domain expiration
+                </p>
+              </div>
+              ) : null}
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 flex flex-col gap-2">
+          {whoisResult['Registrant Organization'] ? (
+            <div className="rounded-xl bg-slate-100 px-8 py-5 dark:bg-slate-950">
+              <div class="flex flex-row justify-between">
+                <span className="rounded-lg bg-slate-200 px-2 py-1 text-sm font-extrabold uppercase text-slate-950 dark:bg-slate-900 dark:text-slate-50">
+                  Domain Owner
+                </span>
+                <ReactCountryFlag countryCode={whoisResult['Registrant Country']} svg style={{
+                  fontSize: '1.75em',
+                  lineHeight: '1.75em',
+                  borderRadius: '20%',
+                }} />
+              </div>
+              <div className="mt-4">
+                <p className="text-2xl font-bold text-slate-900 dark:text-slate-100">
+                  {whoisResult['Registrant Organization']}
+                </p>
+              </div>
+            </div>
+          ) : null}
+          {whoisResult['Registrar'] ? (
+          <div className="rounded-xl bg-slate-100 px-8 py-5 dark:bg-slate-950">
+            <div class="flex flex-row justify-between">
+              <span className="rounded-lg bg-slate-200 px-2 py-1 text-sm font-extrabold uppercase text-slate-950 dark:bg-slate-900 dark:text-slate-50">
+                Domain Registry
+              </span>
+              <ReactCountryFlag countryCode={whoisResult['Registrant Country']} svg style={{
+                    fontSize: '1.75em',
+                    lineHeight: '1.75em',
+                    borderRadius: '20%',
+                }}/>
+            </div>
+            <div className="mt-4">
+              <a href={whoisResult['Registrar URL']} className="text-2xl font-bold text-slate-900 dark:text-slate-100">
+                {whoisResult['Registrar']}
+              </a>
+            </div>
+            </div>
+          ) : null}
+        </div>
+        <div className="relative col-span-1 w-full">
+          <img
+            src="https://www.stuttgarter-nachrichten.de/media.media.129d97af-13ca-4d8b-a2e5-5f0e21903ff0.original1024.jpg"
+            className="h-56 rounded-xl"
+            alt=""
+          />
+
+          <div className="absolute top-0 flex h-56 w-full rounded-xl bg-orange-500 bg-opacity-40">
+            <img
+              className="m-auto h-16"
+              src="https://companieslogo.com/img/orig/NET_BIG-03fc28ae.png?t=1647436387"
+              alt=""
+            />
+          </div>
+
+          <button className="absolute top-0 flex w-full cursor-pointer p-2">
+            <img
+              className="ml-auto h-6 w-6"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Infobox_info_icon.svg/480px-Infobox_info_icon.svg.png"
+              alt=""
+            />
+          </button>
+        </div>
+      </div>
+      <div className="mt-4 grid gap-4 md:grid-cols-5">
+        <div
+          className="relative h-72 w-full rounded-xl bg-cover md:col-span-2"
+          style={{
+            backgroundImage:
+              "url('https://helios-i.mashable.com/imagery/articles/06CN9HpdYJ3zWr2vUfYDFBG/hero-image.fill.size_1248x702.v1623387368.png')",
+          }}
+        ></div>
+        <div className="md:col-span-3">
+          <div className="h-72 rounded-xl bg-slate-100 px-8 py-5 dark:bg-slate-950">
+            <div>
+              <span className="rounded-lg bg-slate-200 px-2 py-1 text-sm font-extrabold uppercase text-slate-950 dark:bg-slate-900 dark:text-slate-50">
+                Preview
+              </span>
+            </div>
+            <div className="mt-16">
+              <p className="text-xl font-bold text-slate-900 dark:text-slate-100">
+                Youtube &middot; Deine
+              </p>
+
+              <div className="mt-3">
+                <p>https://youtube./</p>
+                <p>22 Archive.org snapshots found</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LookupDomain;
