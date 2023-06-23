6bcb91b1e3763b858d852fb06687013140518cda
Mathis <mathis.fullriede@gmail.com>
2023-06-23 15:03:39 +0200

    ui improvements

--- components/domain-not-registered.tsx
+++ components/domain-not-registered.tsx
@@ -6,11 +6,13 @@ import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 
 const DomainNotRegistered = () => (
   <Alert variant="destructive">
-    <XSquareIcon className="h-6 w-6" />
-    <AlertTitle>Not registered</AlertTitle>
-    <AlertDescription>
-      This Domain is currently not registered.
-    </AlertDescription>
+    <XSquareIcon className="h-8 w-8" />
+    <div className="ml-2">
+      <AlertTitle>Not registered</AlertTitle>
+      <AlertDescription>
+        This Domain is currently not registered.
+      </AlertDescription>
+    </div>
   </Alert>
 );
 
