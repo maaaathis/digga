a6177be583699f1590ab46b25b2573561695a176
Mathis <mathis.fullriede@gmail.com>
2023-06-23 23:13:01 +0200

    fix false formatting

--- app/api/screenshot/route.js
+++ app/api/screenshot/route.js
@@ -2,11 +2,16 @@ import captureWebsite from 'capture-website';
 
 export async function GET(req, res) {
   const { searchParams } = new URL(req.url);
-    const url = searchParams.get('url');
+  const url = searchParams.get('url');
 
   // Create a screenshot buffer of the url
   // Use the new headless mode
-  const screenshot = await captureWebsite.buffer(url, { launchOptions: { headless: "new" }, disableAnimations: true, width: 426, height: 240 });
+  const screenshot = await captureWebsite.buffer(url, {
+    launchOptions: { headless: 'new' },
+    disableAnimations: true,
+    width: 426,
+    height: 240,
+  });
 
   // Convert the screenshot buffer to a base64 string
   const screenshotBase64 = screenshot.toString('base64');
--- components/PageScreenshotter.jsx
+++ components/PageScreenshotter.jsx
@@ -1,27 +1,39 @@
+'use client';
 
-"use client"
 import Image from 'next/image';
 import useSWR from 'swr';
 
 // Define a custom fetcher function that calls the API route
-const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = (url) => {
+  // Gib die axios-Anfrage zurück
+  return axios
+    .get(url)
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+};
 
 export default async function PageScreenshotter({ url }) {
-    console.log('URL'+url);
-    // Use useSWR to fetch the screenshot data from the API route
-    const { data, error } = useSWR(`/api/screenshot?url=${url}`, fetcher);
-    
-    console.log(data)
+  console.log('URL' + url);
+  // Use useSWR to fetch the screenshot data from the API route
+  const { data, error } = useSWR(`/api/screenshot?url=${url}`, fetcher);
+
+  console.log(data);
 
   // Return a JSX element that renders the screenshot or a loading message
   return (
     <div>
+      {error && <p>Es gab einen Fehler beim Laden des Screenshots.</p>}
       {data ? (
-        // Use next/image to display the screenshot base64 string as an image
-        <Image src={`data:image/png;base64,${data.screenshot}`} alt="Screenshot" width={800} height={600} />
+        // Verwende next/image, um den Screenshot base64-String als Bild anzuzeigen
+        <Image
+          src={`data:image/png;base64,${data.screenshot}`}
+          alt="Screenshot"
+          width={800}
+          height={600}
+        />
       ) : (
-        // Display a loading message while the screenshot is being fetched
-        <p>Loading screenshot...</p>
+        // Zeige eine Lademeldung an, während der Screenshot geholt wird
+        <p>Lade Screenshot...</p>
       )}
     </div>
   );
