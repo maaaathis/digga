d1e57d1b1f5fa95316b5ba0508c3908b82f9e431
Mathis <mathis.fullriede@gmail.com>
2023-06-24 17:45:00 +0200

    added icons to security tab

--- components/OverviewSecurity.jsx
+++ components/OverviewSecurity.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { CheckIcon, XIcon } from 'lucide-react';
 
 export default async function OverviewSecurity({ domain }) {
   let returnValue = [];
@@ -86,27 +87,20 @@ export default async function OverviewSecurity({ domain }) {
 
   if (robotsData.fileResponse != 404) {
     returnValue.push(
-      <a href={robotsData.domain+"/"+robotsData.file} target="_blank" rel="noopener noreferrer" className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800">
-        <svg
-          className="h-1.5 w-1.5 fill-green-400"
-          viewBox="0 0 6 6"
-          aria-hidden="true"
-        >
-          <circle cx={3} cy={3} r={3} />
-        </svg>
+      <a
+        href={robotsData.domain + '/' + robotsData.file}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800"
+      >
+        <CheckIcon className="h-1.5 w-1.5 fill-green-400" aria-hidden="true" />
         robots.txt
       </a>
     );
   } else {
     returnValue.push(
       <span className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800">
-        <svg
-          className="h-1.5 w-1.5 fill-red-400"
-          viewBox="0 0 6 6"
-          aria-hidden="true"
-        >
-          <circle cx={3} cy={3} r={3} />
-        </svg>
+        <XIcon className="h-1.5 w-1.5 fill-red-400" aria-hidden="true" />
         robots.txt
       </span>
     );
@@ -114,27 +108,20 @@ export default async function OverviewSecurity({ domain }) {
 
   if (securityData.fileResponse != 404) {
     returnValue.push(
-      <a href={securityData.domain+"/"+securityData.file} target="_blank" rel="noopener noreferrer" className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800">
-        <svg
-          className="h-1.5 w-1.5 fill-green-400"
-          viewBox="0 0 6 6"
-          aria-hidden="true"
-        >
-          <circle cx={3} cy={3} r={3} />
-        </svg>
+      <a
+        href={securityData.domain + '/' + securityData.file}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800"
+      >
+        <CheckIcon className="h-1.5 w-1.5 fill-green-400" aria-hidden="true" />
         security.txt
       </a>
     );
   } else {
     returnValue.push(
       <span className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800">
-        <svg
-          className="h-1.5 w-1.5 fill-red-400"
-          viewBox="0 0 6 6"
-          aria-hidden="true"
-        >
-          <circle cx={3} cy={3} r={3} />
-        </svg>
+        <XIcon className="h-1.5 w-1.5 fill-red-400" aria-hidden="true" />
         security.txt
       </span>
     );
@@ -143,26 +130,14 @@ export default async function OverviewSecurity({ domain }) {
   if (hstsData != 'false') {
     returnValue.push(
       <span className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800">
-        <svg
-          className="h-1.5 w-1.5 fill-green-400"
-          viewBox="0 0 6 6"
-          aria-hidden="true"
-        >
-          <circle cx={3} cy={3} r={3} />
-        </svg>
+        <CheckIcon className="h-1.5 w-1.5 fill-green-400" aria-hidden="true" />
         HSTS
       </span>
     );
   } else {
     returnValue.push(
       <span className="mx-1 my-2 inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-800">
-        <svg
-          className="h-1.5 w-1.5 fill-red-400"
-          viewBox="0 0 6 6"
-          aria-hidden="true"
-        >
-          <circle cx={3} cy={3} r={3} />
-        </svg>
+        <XIcon className="h-1.5 w-1.5 fill-red-400" aria-hidden="true" />
         HSTS
       </span>
     );
