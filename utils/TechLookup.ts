7b8d78722676b6b9d87020e45f972ed6744b1ebc
Mathis <mathis.fullriede@gmail.com>
2023-06-24 16:58:14 +0200

    formatted code

--- utils/TechLookup.ts
+++ utils/TechLookup.ts
@@ -1,12 +1,11 @@
 import WebsiteAnalyzer from 'website-analysis-api/src/WebsiteAnalyzer';
 
-import TechStackData from './techstack/technologies.json';
 import CategoryData from './techstack/categories.json';
 import GroupData from './techstack/groups.json';
+import TechStackData from './techstack/technologies.json';
 
 class TechLookup {
   static async fetchTechs(domain: string): Promise<any> {
-
     const analyzer = new WebsiteAnalyzer({
       url: 'https://' + domain + '/',
       technologies: TechStackData,
