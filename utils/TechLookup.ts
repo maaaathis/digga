7dbf2755451367ab12cc6aa49a231a6ac562b211
Mathis <mathis.fullriede@gmail.com>
2023-06-24 16:54:17 +0200

    move to json object data submission

--- utils/TechLookup.ts
+++ utils/TechLookup.ts
@@ -1,15 +1,17 @@
-import fs from 'fs';
 import WebsiteAnalyzer from 'website-analysis-api/src/WebsiteAnalyzer';
+
+import TechStackData from './techstack/technologies.json';
+import CategoryData from './techstack/categories.json';
+import GroupData from './techstack/groups.json';
 
 class TechLookup {
   static async fetchTechs(domain: string): Promise<any> {
-    const path = './utils/techstack/';
 
     const analyzer = new WebsiteAnalyzer({
       url: 'https://' + domain + '/',
-      technologiesFile: path + 'technologies.json',
-      categoriesFile: path + 'categories.json',
-      groupsFile: path + 'groups.json',
+      technologies: TechStackData,
+      categories: CategoryData,
+      groups: GroupData,
     });
 
     const data = await analyzer.analyze();
