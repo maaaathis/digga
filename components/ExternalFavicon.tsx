de6c916c21b26032289e32728f16677fc2975d7a
Mathis <mathis.fullriede@gmail.com>
2023-10-27 21:54:16 +0200

    format

--- components/ExternalFavicon.tsx
+++ components/ExternalFavicon.tsx
@@ -9,7 +9,7 @@ const ExternalFavicon: FC<ExternalFavicon> = ({ url }) => {
   return (
     <Image
       src={`https://www.google.com/s2/favicons?sz=32&domain_url=${url}`}
-      alt={""}
+      alt={''}
       width={32}
       height={32}
       className="my-auto inline-block h-8 w-8 rounded-sm"
