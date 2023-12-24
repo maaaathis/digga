ac5979b76c3c523cd49a87e44f6673206768ef5b
Mathis <mathis.fullriede@gmail.com>
2023-12-24 16:42:07 +0100

    add noRecordsFoundX for nameserver widget

--- components/overview/NameserverWidget.tsx
+++ components/overview/NameserverWidget.tsx
@@ -1,3 +1,4 @@
+import { XSquareIcon } from 'lucide-react';
 import React from 'react';
 import whoiser from 'whoiser';
 
@@ -21,21 +22,27 @@ const NameserverWidget: React.FC<Props> = async ({
 
   return (
     <DashboardItem title="Nameserver">
-      <div className="my-auto">
-        <ul className="list-inside list-disc text-lg font-medium text-slate-900 dark:text-slate-100">
-          {Object.values(whoisResult['Name Server']).map((ns) => {
-            return (
-              <li key={ns as string}>
-                <a
-                  className="cursor-pointer decoration-slate-700 decoration-dotted underline-offset-4 hover:underline dark:decoration-slate-300"
-                  href={`/lookup/${ns as string}`}
-                >
-                  {ns as string}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="flex h-full">
+        {Object.values(whoisResult['Name Server']).length === 0 ? (
+          <div className="m-auto">
+            <XSquareIcon className="h-10 w-10 text-black dark:text-white" />
+          </div>
+        ) : (
+          <ul className="list-inside list-disc text-lg font-medium text-slate-900 dark:text-slate-100">
+            {Object.values(whoisResult['Name Server']).map((ns) => {
+              return (
+                <li key={ns as string}>
+                  <a
+                    className="cursor-pointer decoration-slate-700 decoration-dotted underline-offset-4 hover:underline dark:decoration-slate-300"
+                    href={`/lookup/${ns as string}`}
+                  >
+                    {ns as string}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </DashboardItem>
   );
