ca68bea9b643abf09a5f5d25a39e648079b140a9
Mathis <mathis.fullriede@gmail.com>
2024-01-02 14:43:42 +0100

    remove unnecessary whois request on lookup landing

--- app/lookup/[domain]/page.tsx
+++ app/lookup/[domain]/page.tsx
@@ -1,5 +1,4 @@
 import { FC, ReactElement } from 'react';
-import whoiser from 'whoiser';
 
 import TechnologiesWidget from '@/components/overview/TechnologiesWidget';
 import { DomainAvailability, isAvailable } from '@/lib/whois';
@@ -13,8 +12,6 @@ import DomainlabelWidget from '../../../components/overview/DomainlabelWidget';
 import DomainOwnerInfoWidget from '../../../components/overview/DomainOwnerInfoWidget';
 import NameserverWidget from '../../../components/overview/NameserverWidget';
 
-export const fetchCache = 'default-no-store';
-
 interface LookupDomainProps {
   params: {
     domain: string;
@@ -24,13 +21,6 @@ interface LookupDomainProps {
 const LookupDomain: FC<LookupDomainProps> = async ({
   params: { domain },
 }): Promise<ReactElement> => {
-  // @ts-ignore
-  const whoisResult = whoiser.firstResult(
-    await whoiser(domain, {
-      timeout: 3000,
-    })
-  );
-
   if ((await isAvailable(domain)) !== DomainAvailability.REGISTERED) {
     return <DomainNotRegistered />;
   }
