214b17859de9cc56a098e6a46fd24991a1562140
Levi Heßmann <levihsmn@gmail.com>
2024-01-02 01:15:13 +0100

    feat: Add new technologies widget for overview (#60)
    * feat: add widget for technologies
    
    * refactor: changed icon to grip, added revalidation to fetch, fixed color on buttons, added margin to modal, removed x overflow scroll
    
    * Update components/overview/TechnologiesWidget.tsx
    
    Co-authored-by: mathis <37186532+maaaathis@users.noreply.github.com>
    
    * feature: add data provided link
    
    * add tooltip for technology name in widget
    
    * adjust codestyle
    
    ---------
    
    Co-authored-by: mathis <37186532+maaaathis@users.noreply.github.com>
    Co-authored-by: Mathis <mathis.fullriede@gmail.com>

---
+++ components/TechnologiesDetailsModal.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import { GripIcon, MoreHorizontal } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { type FC, ReactElement } from 'react';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Spinner } from '@/components/ui/spinner';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
+
+type Technology = {
+  name: string;
+  category: number;
+  icon: string;
+  index: string;
+  sourceUrl: string;
+  detectedTime: number;
+  latestDetectedTime: number;
+  website: string;
+  siteListUrl: string;
+  categoryString?: string;
+};
+
+type TechnologiesDetailsProps = {
+  technologies: Technology[];
+};
+
+const TechnologiesDetailsModal: FC<TechnologiesDetailsProps> = ({
+  technologies,
+}): ReactElement => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <div
+        key={'more'}
+        onClick={() => setOpen(true)}
+        className="flex h-12 w-12 justify-center rounded-lg hover:cursor-pointer hover:bg-black/5 dark:hover:bg-white/5"
+      >
+        <GripIcon className="m-auto h-5 w-5 text-black dark:text-white" />
+      </div>
+
+      <Dialog modal open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="mb-4">Used Technologies</DialogTitle>
+            <DialogDescription className="max-h-96 overflow-scroll overflow-x-hidden">
+              <Table>
+                <TableBody>
+                  {technologies.map((technology: Technology) => {
+                    return (
+                      <TableRow key={technology.index}>
+                        <TableCell>
+                          <Image
+                            src={`https://www.whatruns.com/imgs/${technology.icon}`}
+                            alt={technology.name}
+                            width={20}
+                            height={20}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <a
+                            className="cursor-pointer select-none decoration-slate-700 decoration-dotted underline-offset-4 hover:underline dark:decoration-slate-300"
+                            href={technology.website}
+                          >
+                            {technology.name}
+                          </a>
+                        </TableCell>
+                        <TableCell>{technology.categoryString}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+
+              <div className="mt-4">
+                <p className="text-xs text-opacity-80">
+                  Data provided by{' '}
+                  <Link
+                    href="https://www.whatruns.com/"
+                    className="cursor-pointer select-none decoration-slate-700 decoration-dotted underline-offset-4 hover:underline dark:decoration-slate-300"
+                  >
+                    WhatRuns.com
+                  </Link>
+                  .
+                </p>
+              </div>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default TechnologiesDetailsModal;
