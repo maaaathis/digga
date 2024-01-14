c4b9d6d63e7f609b5a35148c4a4183fbec4648a1
Mathis <mathis.fullriede@gmail.com>
2024-01-14 15:55:51 +0100

    debugging

--- app/lookup/[domain]/loading.tsx
+++ app/lookup/[domain]/loading.tsx
@@ -5,7 +5,7 @@ import { Skeleton } from '@/components/ui/skeleton';
 const OverviewLoader: FC = (): ReactElement => (
   <div className="flex flex-col gap-4 md:grid md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
     {Array.from({ length: 8 }).map((_, i) => (
-      <Skeleton className="col-span-2 h-56 w-full rounded-sm" key={i} />
+      <Skeleton className="col-span-2 h-56 w-full rounded-xl" key={i} />
     ))}
   </div>
 );
