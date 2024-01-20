9045290df58a9e05bb15ae90225fec905d70a80b
Mathis <mathis.fullriede@gmail.com>
2024-01-20 21:54:56 +0100

    simple logic

--- app/lookup/[domain]/archive/page.tsx
+++ app/lookup/[domain]/archive/page.tsx
@@ -1,4 +1,5 @@
 import { CircleOff } from 'lucide-react';
+import { DateTime } from 'luxon';
 import { Metadata } from 'next';
 import { type FC, ReactElement } from 'react';
 
@@ -33,6 +34,111 @@ const ArchivePage: FC<ArchivePageProps> = async ({
   const latestSnapshot = await latestSnapshotFetch.json();
 
   console.log(latestSnapshot);
+
+  async function fetchData(
+    timestamp,
+    resultArray,
+    domain,
+    callback,
+    gotDifferenceInMonths
+  ) {
+    const formattedTimestamp = DateTime.fromFormat(
+      timestamp,
+      'yyyyMMddHHmmss'
+    ).toFormat('yyyyMMddHHmmss');
+    const url = `https://archive.org/wayback/available?url=${domain}&timestamp=${formattedTimestamp}`;
+
+    try {
+      const latestSnapshotFetch = await fetch(url);
+      const responseData = await latestSnapshotFetch.json();
+
+      if (
+        responseData.archived_snapshots &&
+        responseData.archived_snapshots.closest
+      ) {
+        const closestTimestamp =
+          responseData.archived_snapshots.closest.timestamp;
+
+        if (responseData.archived_snapshots.closest.timestamp === timestamp) {
+          if (gotDifferenceInMonths > 48) return;
+
+          const nextTimestamp = DateTime.fromFormat(
+            closestTimestamp,
+            'yyyyMMddHHmmss'
+          ).minus({ months: gotDifferenceInMonths * 2 });
+
+          const formattedNextTimestamp =
+            nextTimestamp.toFormat('yyyyMMddHHmmss');
+
+          await fetchData(
+            formattedNextTimestamp,
+            resultArray,
+            domain,
+            callback,
+            gotDifferenceInMonths * 2
+          );
+        }
+
+        resultArray.push(responseData);
+
+        if (callback) {
+          callback(resultArray);
+        }
+
+        // Berechnen Sie den nächsten Timestamp basierend auf der Differenz zum vorherigen
+        let nextTimestamp;
+        const differenceInMonths = DateTime.fromFormat(
+          closestTimestamp,
+          'yyyyMMddHHmmss'
+        ).diff(
+          DateTime.fromFormat(timestamp, 'yyyyMMddHHmmss'),
+          'months'
+        ).months;
+
+        if (differenceInMonths > -6) {
+          // Wenn die Differenz größer als -6 Monate ist, setze den nächsten Timestamp auf -6 Monate
+          nextTimestamp = DateTime.fromFormat(
+            closestTimestamp,
+            'yyyyMMddHHmmss'
+          ).minus({ months: 6 });
+        } else {
+          // Andernfalls setze den nächsten Timestamp auf -1 Monat
+          nextTimestamp = DateTime.fromFormat(
+            closestTimestamp,
+            'yyyyMMddHHmmss'
+          ).minus({ months: 1 });
+        }
+
+        const formattedNextTimestamp = nextTimestamp.toFormat('yyyyMMddHHmmss');
+
+        await fetchData(
+          formattedNextTimestamp,
+          resultArray,
+          domain,
+          callback,
+          differenceInMonths
+        );
+      } else {
+        // Stoppen Sie die Rekursion, wenn keine älteren Snapshots mehr gefunden werden
+        console.log('Keine älteren Snapshots gefunden.');
+      }
+    } catch (error) {
+      // Fehlerbehandlung hier, wenn die Anfrage fehlschlägt
+      console.error('Fehler bei der Anfrage:', error);
+    }
+  }
+
+  const resultArray = [];
+
+  fetchData(
+    latestSnapshot.archived_snapshots.closest.timestamp,
+    resultArray,
+    domain,
+    (finalResult) => {
+      console.log('Endgültiges Ergebnis:', finalResult);
+    },
+    1
+  );
 
   if (!latestSnapshot.archived_snapshots.closest) {
     return (
