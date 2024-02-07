ed8a53e08b525f05b836237356097060e3f525d6
mathis <37186532+maaaathis@users.noreply.github.com>
2024-02-07 23:19:53 +0100

    add Google DNS flush & DNS History button (#175)
    * add Google DNS flush & DNS History button
    
    * cleanup unused imports

---
+++ app/lookup/[domain]/dns/_components/DnsHistoryButton.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { ExternalLink } from 'lucide-react';
+import Link from 'next/link';
+import { FC } from 'react';
+
+import { Button } from '@/components/ui/button';
+
+interface Props {
+  domain: string;
+}
+
+export const DnsHistoryButton: FC<Props> = (props) => {
+  const { domain } = props;
+
+  return (
+    <Link
+      href={`https://dnshistory.org/dns-records/${domain}`}
+      target="_blank"
+      rel="noopener"
+      className="my-auto"
+    >
+      <Button variant="outline" className="rounded-lg text-sm font-medium">
+        DNS History
+        <ExternalLink className="ml-2 h-3 w-3" />
+      </Button>
+    </Link>
+  );
+};
+
+export default DnsHistoryButton;
