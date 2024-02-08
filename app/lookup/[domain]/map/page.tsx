68d8c3b801535aadcfe6a894401e839477dc4310
Mathis <mathis.fullriede@gmail.com>
2024-02-08 22:20:58 +0100

    optimize layout

--- app/lookup/[domain]/map/page.tsx
+++ app/lookup/[domain]/map/page.tsx
@@ -2,6 +2,7 @@ import { isbot } from 'isbot';
 import { CheckCircleIcon, InfoIcon, ShieldAlertIcon } from 'lucide-react';
 import type { Metadata } from 'next';
 import { headers } from 'next/headers';
+import Link from 'next/link';
 import type { FC } from 'react';
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
@@ -70,14 +71,18 @@ const MapResultsPage: FC<MapResultsPageProps> = async ({
   );
 
   const regionEntries = markers.map((marker) => (
-    <div key={marker.code}>
+    <Link
+      href={`/lookup/${domain}?resolver=cloudflare&location=${marker.code}`}
+      key={marker.code}
+      className="rounded-xl border border-gray-200 p-4 transition-colors duration-200 ease-in-out hover:bg-gray-100"
+    >
       <h3 className="mb-2 font-semibold">{marker.name}</h3>
       <ul>
         {marker.results.A.map((ip, index) => (
           <li key={index}>{ip}</li>
         ))}
       </ul>
-    </div>
+    </Link>
   ));
 
   let hasDifferentRecords = false;
@@ -98,34 +103,34 @@ const MapResultsPage: FC<MapResultsPageProps> = async ({
 
   return (
     <>
-      <Alert className="mx-auto mt-16 max-w-max">
-        {hasDifferentRecords ? (
-          <>
-            <InfoIcon className="h-4 w-4" />
-            <AlertTitle>Different records detected!</AlertTitle>
-            <AlertDescription>
-              Not all regions have the same records for this domain. This{' '}
-              <i>could</i> be an indication for the use of GeoDNS.
-              <br /> Keep in mind however, that some providers rotate their IP
-              addresses, which can also lead to different results.
-            </AlertDescription>
-          </>
-        ) : (
-          <>
-            <CheckCircleIcon className="h-4 w-4" />
-            <AlertTitle>All records are the same!</AlertTitle>
-            <AlertDescription>
-              All records are the same for all regions. Therefore propagation
-              was successful and the domain is not using GeoDNS.
-            </AlertDescription>
-          </>
-        )}
-      </Alert>
-      <div className="flex flex-row">
+      <div className="flex flex-col md:flex-row">
         <div className="basis-2/5">
           <div className="grid grid-cols-2 gap-4">{regionEntries}</div>
         </div>
         <div className="basis-3/5">
+          <Alert className="mx-auto">
+            {hasDifferentRecords ? (
+              <>
+                <InfoIcon className="h-4 w-4" />
+                <AlertTitle>Different records detected!</AlertTitle>
+                <AlertDescription>
+                  Not all regions have the same records for this domain. This{' '}
+                  <i>could</i> be an indication for the use of GeoDNS.
+                  <br /> Keep in mind however, that some providers rotate their
+                  IP addresses, which can also lead to different results.
+                </AlertDescription>
+              </>
+            ) : (
+              <>
+                <CheckCircleIcon className="h-4 w-4" />
+                <AlertTitle>All records are the same!</AlertTitle>
+                <AlertDescription>
+                  All records are the same for all regions. Therefore
+                  propagation was successful and the domain is not using GeoDNS.
+                </AlertDescription>
+              </>
+            )}
+          </Alert>
           <ResultsGlobe domain={domain} markers={markers} />
         </div>
       </div>
