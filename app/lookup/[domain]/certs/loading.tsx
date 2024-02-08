331dfff600aac5ca1c3a7715b1b1f45dc034d012
mathis <37186532+maaaathis@users.noreply.github.com>
2024-02-08 21:02:09 +0100

    load certs on the edge & add custom certs loadingpage (#181)
    * load certs on the edge & custom certs loadingpage
    
    * Add return type

--- app/lookup/[domain]/certs/loading.tsx
+++ app/lookup/[domain]/certs/loading.tsx
@@ -1,13 +1,54 @@
-import { FC, ReactElement } from 'react';
+'use client';
+
+import { ReactElement } from 'react';
 
 import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
-const CertsLoading: FC = (): ReactElement => (
-  <div className="mt-12">
-    {Array.from({ length: 10 }).map((_, i) => (
-      <Skeleton className="my-3 h-5 w-full rounded-sm" key={i} />
-    ))}
-  </div>
+const CertsLoading = (): ReactElement => (
+  <Table>
+    <TableHeader>
+      <TableRow className="hover:bg-transparent">
+        <TableHead className="pl-0">Logged At</TableHead>
+        <TableHead>Not Before</TableHead>
+        <TableHead>Not After</TableHead>
+        <TableHead>Common Name</TableHead>
+        <TableHead>Matching Identities</TableHead>
+        <TableHead className="pr-0">Issuer Name</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      {Array.from({ length: 8 }).map((_, i) => (
+        <TableRow key={i} className="hover:bg-transparent">
+          <TableCell className="pl-0">
+            <Skeleton className="h-5 w-44 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-44 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-44 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-48 rounded-sm" />
+          </TableCell>
+          <TableCell>
+            <Skeleton className="h-5 w-48 rounded-sm" />
+          </TableCell>
+          <TableCell className="pr-0">
+            <Skeleton className="h-5 w-64 rounded-sm" />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
 );
 
 export default CertsLoading;
