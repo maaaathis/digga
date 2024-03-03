7d5598a65e02393d72675941adc01dea5f9afcb1
Mathis <mathis.fullriede@gmail.com>
2024-03-03 21:34:48 +0100

    center title on small devices

--- app/lookup/[domain]/error.tsx
+++ app/lookup/[domain]/error.tsx
@@ -15,7 +15,7 @@ const DomainError: FC<DomainErrorProps> = ({ error }): ReactElement => {
   return (
     <div className="mt-12 flex flex-col items-center gap-2">
       <ServerCrash className="h-12 w-12" />
-      <h2 className="mt-1 font-clash text-3xl font-bold tracking-wide">
+      <h2 className="mt-1 text-center font-clash text-3xl font-bold tracking-wide">
         Something went wrong!
       </h2>
       <p className="mt-1 text-center text-sm text-muted-foreground">
