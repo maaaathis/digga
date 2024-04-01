4e03f881519f79ac34a7b02e3bb15c3910e6cde2
Mathis <mathis.fullriede@gmail.com>
2024-04-01 14:26:38 +0200

    refactor: lookup-ip api route

--- lib/ips.ts
+++ lib/ips.ts
@@ -28,15 +28,32 @@ export const getIpDetails = async (ip: string) => {
   return data as IpDetails;
 };
 
-const normalizeIp = (ip: string) =>
-  ip.replace(/\.\d+$/, '.0').replace(/:([0-9a-fA-F]+)$/, ':');
+export const getIpGreenDetails = async (ip: string): Promise<boolean> => {
+  const response = await fetch(
+    `https://api.thegreenwebfoundation.org/greencheck/${ip}`
+  );
+
+  if (!response.ok)
+    throw new Error(`Error fetching GREEN-IP details: ${response.statusText}`);
+
+  const data = (await response.json()) as Record<string, any>;
+  delete data.status;
+
+  return data.green;
+};
+
+// Standardize last segment of IP address to reduce the number of requests and avoid rate limiting
+// 1st Regex is for IPv4
+// 2nd Regex is for IPv6
+export const normalizeIpEnding = (ip: string) =>
+  ip.replace(/\.[0-9]+$/, '.0').replace(/:([0-9a-fA-F]+)$/, ':');
 
 // Normalize IPs to their CIDR ranges to reduce the number of requests and avoid rate limiting
 export const hostLookupLoader = new DataLoader(
   async (keys: readonly string[]) =>
     Promise.all(
       keys.map(async (ip) => {
-        const normalIp = normalizeIp(ip);
+        const normalIp = normalizeIpEnding(ip);
         const data = await getIpDetails(normalIp);
 
         if (data.org === data.isp) {
@@ -47,7 +64,7 @@ export const hostLookupLoader = new DataLoader(
       })
     ),
   {
-    cacheKeyFn: normalizeIp,
+    cacheKeyFn: normalizeIpEnding,
   }
 );
 
