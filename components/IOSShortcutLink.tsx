aeb182a80f677e4849d6fd7705e041c28a5d876a
Mathis <mathis.fullriede@gmail.com>
2024-05-10 21:50:48 +0200

    feat: add iOS shortcut

---
+++ components/IOSShortcutLink.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { FC, ReactElement } from 'react';
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+
+const IOSShortcutLink: FC = (): ReactElement => {
+  return (
+    <div className="flex justify-center">
+      <a
+        className="select-none rounded-lg bg-slate-200 p-2 px-4 text-center duration-300 hover:scale-105 hover:cursor-pointer dark:bg-secondary"
+        href="https://www.icloud.com/shortcuts/7d45a70403ec4bf783d32c45ad61b2d1"
+        target="_blank"
+        rel="noopener"
+      >
+        Add shortcut
+      </a>
+    </div>
+  );
+};
+
+export default IOSShortcutLink;
