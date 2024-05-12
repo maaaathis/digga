94762068490560794def16b9e6d0f0d68ab625af
Mathis <mathis.fullriede@gmail.com>
2024-05-12 19:25:31 +0200

    feat: add insights

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import PlausibleProvider from 'next-plausible';
 import { ThemeProvider } from 'next-themes';
 import type { FC, ReactElement, ReactNode } from 'react';
 import { SWRConfig } from 'swr';
@@ -8,12 +9,28 @@ type ProvidersProps = {
   children: ReactNode;
 };
 
+const DiggaPlausibleProvider: FC<ProvidersProps> = ({children}) => {
+  if (!process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN) {
+    return children;
+  }
+
+  return (
+    <PlausibleProvider
+      customDomain={process.env.NEXT_PUBLIC_PLAUSIBLE_HOST}
+      trackOutboundLinks
+      domain={process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN}
+    >
+      {children}
+    </PlausibleProvider>
+  );
+};
+
 const Providers: FC<ProvidersProps> = ({ children }): ReactElement => (
   <ThemeProvider attribute="class">
     <SWRConfig
       value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
     >
-      {children}
+      <DiggaPlausibleProvider>{children}</DiggaPlausibleProvider>
     </SWRConfig>
   </ThemeProvider>
 );
