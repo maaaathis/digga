eef68eeb39da050174384141202ae08615bbcb26
mathis <37186532+maaaathis@users.noreply.github.com>
2026-05-28 08:55:54 +0200

    fix(sitemap): correct urlset namespace in sitemap-common.xml

--- src/app/sitemap-common.xml/route.ts
+++ src/app/sitemap-common.xml/route.ts
@@ -28,7 +28,7 @@ export async function GET() {
 	);
 
 	const body = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap-0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls
 	.map(
 		url => `\t<url>
